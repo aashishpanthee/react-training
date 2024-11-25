50a380d82465799c8c00ae223417714c02d0e2f1
Aashish Panthee <aashishpanthee060@gmail.com>
2024-11-25 11:41:00 +0545

    moved code of script tag to external script file

--- App.js
+++ App.js
@@ -1,1 +1,10 @@
-console.log('hello world');
+const child = React.createElement('div', { id: 'child' }, [
+  React.createElement('h1', { id: 'heading' }, 'Hello World'),
+  React.createElement('h2', { id: 'heading2' }, 'Hello World2'),
+  React.createElement('div', { id: 'child3' }, [
+    React.createElement('h1', { id: 'heading' }, 'Hello World'),
+    React.createElement('h2', { id: 'heading2' }, 'Hello World2'),
+  ]),
+]);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(child);
