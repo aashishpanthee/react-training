96a0a63f17c9689cec3bff9e941863cb33436394
Aashish Panthee <aashishpanthee060@gmail.com>
2024-11-26 11:54:42 +0545

    parcel setup & npm installed

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
-const child = React.createElement('div', { id: 'child' }, [
-  React.createElement('h1', { id: 'heading' }, 'Hello World'),
-  React.createElement('h2', { id: 'heading2' }, 'Hello World2'),
-  React.createElement('div', { id: 'child3' }, [
-    React.createElement('h1', { id: 'heading' }, 'Hello World'),
-    React.createElement('h2', { id: 'heading2' }, 'Hello World2'),
-  ]),
-]);
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const Title = () => {
+  return <h2>Hello world 2</h2>;
+};
+const Child = () => (
+  <div>
+    <h1>Hello world 1</h1>
+    <Title />
+  </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(child);
+root.render(<Child />);
---
+++ readme.md
@@ -0,0 +1,10 @@
+Job of Parcel
+
+- Makes development build of our project
+- Hosts the development onto the server
+- HMR (Hot Module Replacement)
+- Faster Build (Caching)
+- Image Optimization
+- Minification of File
+- Bundling
+- Compressing
